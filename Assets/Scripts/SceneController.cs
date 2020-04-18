cf8a118ddb4e6e70ee0be4e5c4269f06f90ee3c3
Jason <63879472+fyreflyte@users.noreply.github.com>
2020-04-17 19:30:18 -0700

    Second Checkin in
    Initial assets, logo, font, and start menu structure.

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    public void LoadNewScene(int sceneNum)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneNum);
+    }
+}
